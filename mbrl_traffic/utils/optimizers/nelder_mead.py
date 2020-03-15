96c9a4fdae774c1234912c0f063cface892d65e2
AboudyKreidieh <akreidieh@gmail.com>
2020-03-15 14:32:01 -0700

    cleaned up the solve method

--- mbrl_traffic/utils/optimizers/nelder_mead.py
+++ mbrl_traffic/utils/optimizers/nelder_mead.py
@@ -4,7 +4,17 @@ import scipy.optimize as optimize
 
 
 class NelderMead(Optimizer):
-    """Nelder Mead optimizer object."""
+    """Nelder Mead optimizer object.
+
+    Attributes
+    ----------
+    x0 : array_like
+        the initial guess for the solver
+    bnds : (array_like, array_like)
+        lower and upper bounds for each other parameters
+    options : dict
+        additional solver options that can be passed in
+    """
 
     def __init__(self,
                  param_low,
@@ -12,15 +22,11 @@ class NelderMead(Optimizer):
                  fitness_fn,
                  x0,
                  verbose=2,
-                 maxiter=10000,
-                 maxfev=None,
                  disp=True,
                  initial_simplex=None,
-                 xatol=10e-8,
+                 xatol=1e-8,
                  fatol=1000,
-                 adaptive=False,):
-        """FIXME: x0 should be initial guesses. How will these be passed in"""
-
+                 adaptive=False):
         """Instantiate the optimizer.
 
         Parameters
@@ -36,27 +42,20 @@ class NelderMead(Optimizer):
             the verbosity flag
         disp : bool
             Set to True to print convergence messages.
-        maxiter, maxfev : int
-            Maximum allowed number of iterations and function evaluations. 
-            Will default to N*200, where N is the number of variables, 
-            if neither maxiter or maxfev is set. 
-            If both maxiter and maxfev are set, minimization 
-            will stop at the first reached.
-        initial_simplex : array_like of shape (N + 1, N)
+        initial_simplex : array_like of shape (N+1, N)
             Initial simplex. If given, overrides x0. initial_simplex[j,:] 
-            should contain the coordinates of the j-th vertex of
-             the N+1 vertices in the simplex, where N is the dimension.
-        xatol : float, optional
-            Absolute error in xopt between iterations 
-            that is acceptable for convergence.
-        fatol : number, optional
-            Absolute error in func(xopt) between 
-            iterations that is acceptable for convergence.
-        adaptive : bool, optional
-            Adapt algorithm parameters to dimensionality 
-            of problem. Useful for high-dimensional minimization
+            should contain the coordinates of the j-th vertex of the N+1
+            vertices in the simplex, where N is the dimension.
+        xatol : float
+            Absolute error in xopt between iterations that is acceptable for
+            convergence.
+        fatol : float
+            Absolute error in func(xopt) between iterations that is acceptable
+            for convergence.
+        adaptive : bool
+            Adapt algorithm parameters to dimensionality of problem. Useful
+            for high-dimensional minimization
         """
-
         super(NelderMead, self).__init__(
             param_low=param_low,
             param_high=param_high,
@@ -66,24 +65,26 @@ class NelderMead(Optimizer):
 
         self.x0 = x0
         self.bnds = (param_low, param_high)
-        self.options = {'maxiter': maxiter,
-                        'maxfev': maxfev,
-                        'disp': disp,
-                        'initial_simplex': initial_simplex,
-                        'xatol': xatol,
-                        'fatol': fatol,
-                        'adaptive': adaptive}
+        self.options = {
+            'disp': disp,
+            'initial_simplex': initial_simplex,
+            'xatol': xatol,
+            'fatol': fatol,
+            'adaptive': adaptive
+        }
 
     def solve(self, num_steps=1000, termination_fn=None):
         """See parent class."""
+        # specify the number of steps this will run
+        self.options['maxiter'] = num_steps
 
-        sol = optimize.minimize(self.fitness_fn,
-                                self.x0,
-                                method="Nelder-Mead",
-                                bounds=self.bnds,
-                                callback=None,
-                                options=self.options)
+        sol = optimize.minimize(
+            self.fitness_fn,
+            self.x0,
+            method="Nelder-Mead",
+            bounds=self.bnds,
+            callback=None,
+            options=self.options
+        )
 
         return sol.x
-
-
