1f3f26b8350ea1f581a941adce7a3aa2635dc1cc
AboudyKreidieh <akreidieh@gmail.com>
2020-03-21 20:14:21 -0700

    bug fixes to SAC and proper saving

--- mbrl_traffic/algorithm.py
+++ mbrl_traffic/algorithm.py
@@ -242,7 +242,6 @@ class ModelBasedRLAlgorithm(object):
         self.epoch_q1_losses = []
         self.epoch_q2_losses = []
         self.epoch_value_losses = []
-        self.epoch_alphas = []
         self.epoch_actions = []
         self.epoch_q1s = []
         self.epoch_q2s = []
@@ -316,6 +315,7 @@ class ModelBasedRLAlgorithm(object):
             with self.sess.as_default():
                 self.sess.run(tf.compat.v1.global_variables_initializer())
                 self.policy_tf.initialize()
+                self.model_tf.initialize()
 
             return tf.compat.v1.get_collection(
                 tf.compat.v1.GraphKeys.TRAINABLE_VARIABLES)
@@ -359,13 +359,7 @@ class ModelBasedRLAlgorithm(object):
 
         return action.flatten(), q_value
 
-    def _store_transition(self,
-                          obs0,
-                          action,
-                          reward,
-                          obs1,
-                          terminal1,
-                          evaluate=False):
+    def _store_transition(self, obs0, action, reward, obs1, terminal1):
         """Store a transition in the replay buffer.
 
         Parameters
@@ -380,15 +374,17 @@ class ModelBasedRLAlgorithm(object):
             the current observation
         terminal1 : bool
             is the episode done
-        evaluate : bool
-            whether the sample is being provided by the evaluation environment.
-            If so, the data is not stored in the replay buffer.
         """
         # Scale the rewards by the provided term.
         reward *= self.reward_scale
 
-        self.policy_tf.store_transition(
-            obs0, action, reward, obs1, terminal1, evaluate)
+        self.replay_buffer.add(
+            obs_t=obs0,
+            action=action,
+            reward=reward,
+            obs_tp1=obs1,
+            done=terminal1,
+        )
 
     def learn(self,
               total_timesteps,
@@ -550,6 +546,7 @@ class ModelBasedRLAlgorithm(object):
             Prefix of filenames created for the checkpoint
         """
         save_path = "{}-{}".format(save_path, self.total_steps)
+        ensure_dir(save_path)
         self.model_tf.save(save_path)
         self.policy_tf.save(save_path)
 
@@ -703,13 +700,9 @@ class ModelBasedRLAlgorithm(object):
                 obs, eval_r, done, info = env.step(eval_action)
 
                 # FIXME: maybe add a store_eval option
-                # Store a transition in the replay buffer. This is just for the
-                # purposes of calling features in the store_transition method
-                # of the policy.
-                # self.replay_buffer.store(
-                #     eval_obs, eval_action,
-                #     eval_r, obs, False, evaluate=True
-                # )
+                # Store a transition in the replay buffer.
+                # self._store_transition(
+                #     eval_obs, eval_action, eval_r, obs, done)
 
                 # Update the previous step observation.
                 eval_obs = obs.copy()
@@ -782,7 +775,6 @@ class ModelBasedRLAlgorithm(object):
         if self.policy == SACPolicy:
             combined_stats.update({
                 # Rollout statistics.
-                'rollout/alpha_mean': np.mean(self.epoch_alphas),
                 'rollout/Q1_mean': np.mean(self.epoch_q1s),
                 'rollout/Q2_mean': np.mean(self.epoch_q2s),
                 'rollout/value_mean': np.mean(self.epoch_values),
--- mbrl_traffic/policies/sac.py
+++ mbrl_traffic/policies/sac.py
@@ -2,6 +2,7 @@
 import tensorflow as tf
 import numpy as np
 from functools import reduce
+import os
 
 from mbrl_traffic.policies.base import Policy
 from mbrl_traffic.utils.tf_util import layer
@@ -241,8 +242,8 @@ class SACPolicy(Policy):
 
         # Create the target update operations.
         init, soft = get_target_updates(
-            get_trainable_vars('model/value_fns/vf'),
-            get_trainable_vars('target/value_fns/vf'),
+            get_trainable_vars('policy/model/value_fns/vf'),
+            get_trainable_vars('policy/target/value_fns/vf'),
             tau, verbose)
         self.target_init_updates = init
         self.target_soft_updates = soft
@@ -261,6 +262,13 @@ class SACPolicy(Policy):
         # Setup the running means and standard deviations of the model inputs
         # and outputs.
         self._setup_stats()
+
+        # =================================================================== #
+        # Step 5: Create a saver object to store the model parameters.        #
+        # =================================================================== #
+
+        self.saver = tf.compat.v1.train.Saver(
+            get_trainable_vars("policy/"), max_to_keep=10)
 
     def make_actor(self, obs, action, reuse=False, scope="pi"):
         """Create the actor variables.
@@ -446,7 +454,7 @@ class SACPolicy(Policy):
         if self.verbose >= 2:
             print('setting up critic optimizer')
 
-        scope_name = 'model/value_fns'
+        scope_name = 'policy/model/value_fns'
 
         if self.verbose >= 2:
             for name in ['qf1', 'qf2', 'vf']:
@@ -502,7 +510,7 @@ class SACPolicy(Policy):
         if self.verbose >= 2:
             print('setting up actor and alpha optimizers')
 
-        scope_name = 'model/pi/'
+        scope_name = 'policy/model/pi/'
 
         if self.verbose >= 2:
             actor_shapes = [var.get_shape().as_list()
@@ -681,10 +689,10 @@ class SACPolicy(Policy):
 
         return td_map
 
-    def save(self, save_path):  # TODO
+    def save(self, save_path):
         """See parent class."""
-        raise NotImplementedError
+        self.saver.save(self.sess, os.path.join(save_path, "policy"))
 
-    def load(self, load_path):  # TODO
+    def load(self, load_path):
         """See parent class."""
-        raise NotImplementedError
+        self.saver.restore(self.sess, load_path)
