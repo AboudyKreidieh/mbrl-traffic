b0b17966354a79a359d59f34c2d8795ba3dc071d
Yasharzf <y.zeinali@gmail.com>
2020-03-24 14:09:56 -0700

    added tests for fcnet model

--- tests/fast_tests/test_models.py
+++ tests/fast_tests/test_models.py
@@ -1,5 +1,7 @@
 """Tests for the files in the mbrl_traffic/models folder."""
 import unittest
+import tensorflow as tf
+from gym.spaces import Box
 
 from mbrl_traffic.models.base import Model
 
@@ -41,7 +43,40 @@ class TestFeedForwardModel(unittest.TestCase):
     """Tests the FeedForwardModel object."""
 
     def setUp(self):
-        pass  # TODO
+        self.policy_params = {
+            'sess': tf.compat.v1.Session(),
+            'ac_space': Box(low=-1, high=1, shape=(1,), dtype=np.float32),
+            'ob_space': Box(low=-2, high=2, shape=(2,), dtype=np.float32),
+            'verbose': 2,
+        }
+
+
+        replay_buffer,
+        verbose,
+        model_lr,
+        layer_norm,
+        layers,
+        act_fun,
+        stochastic,
+        num_ensembles)
+
+        FEEDFORWARD_PARAMS = dict(
+        # the max number of transitions to store
+        buffer_size = 200000,
+        # the size of the batch for learning the policy
+        batch_size = 128,
+        # learning rate
+        model_lr = 3e-4,
+        # enable layer normalisation
+        layer_norm = False,
+        # the size of the neural network for the policy
+        layers = [256, 256],
+        # the activation function to use in the neural network
+        act_fun = tf.nn.relu,
+        # whether the output from the model is stochastic or deterministic
+        use_huber = False,
+
+    )
 
     def tearDown(self):
         pass  # TODO
