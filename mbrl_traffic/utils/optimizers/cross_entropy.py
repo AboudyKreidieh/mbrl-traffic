9c467b612ed91a0ec6235db46b7fa0c20c192965
gilbertbahati <bahati95@berkeley.edu>
2020-03-24 00:57:51 -0700

    cleaned up code: documentation needed

--- mbrl_traffic/utils/optimizers/cross_entropy.py
+++ mbrl_traffic/utils/optimizers/cross_entropy.py
@@ -1,7 +1,6 @@
 """Script containing the cross-entropy method optimizer."""
 from mbrl_traffic.utils.optimizers.base import Optimizer
 import numpy as np
-import random
 
 class CrossEntropyMethod(Optimizer):  # TODO
     """Cross-entropy method optimizer object. Adopted from
@@ -11,13 +10,13 @@ class CrossEntropyMethod(Optimizer):  # TODO
                  param_low,
                  param_high,
                  fitness_fn,
-                 x0,
+                 dim,
                  verbose=2,
                  samples=5,
                  ne=10,
                  argmin=True,
                  init_scale=1,
-                 samplemethod='Gaussian'):
+                 samplemethod='Uniform'):
         """Instantiate the optimizer.
         Parameters
         ----------
@@ -38,14 +37,13 @@ class CrossEntropyMethod(Optimizer):  # TODO
             verbose=verbose
         )
         self.func = fitness_fn  # target function
-        self.d = len(x0)  # dimension of function input X: len(x0)
+        self.d = dim  # dimension of function input X: len(x0)
         self.N = samples  # sample N examples each iteration
         self.Ne = ne  # using better Ne examples to update mu and sigma
         self.reverse = not argmin  # try to maximum or minimum the target function (not argmin finds the minimum)
         self.v_min = param_low  # the value minimum
         self.v_max = param_high  # the value maximum
         self.init_coef = init_scale  # sigma initial value
-        self.x0 = x0  # np.array like
 
         assert samplemethod == 'Gaussian' or samplemethod == 'Uniform'
         self.sampleMethod = samplemethod  # which sample method gaussian or uniform, default to gaussian
@@ -55,65 +53,65 @@ class CrossEntropyMethod(Optimizer):  # TODO
         # specify the number of steps this will run
         self.maxits = num_steps
 
-        sol = self.eval(self.x0)
+        sol = self.eval()
 
         return sol, self.func(sol)
 
-    def eval(self, instr):
-        """evalution and return the solution"""
+    def eval(self):
+        """evaluation and return the solution"""
         if self.sampleMethod == 'Gaussian':
-            return self.evalGaussian(instr)
+            return self.eval_gaussian()
         elif self.sampleMethod == 'Uniform':
-            return self.evalUniform(instr)
+            return self.eval_uniform()
 
-    def evalUniform(self, instr):
+    def eval_uniform(self):
 
-        t, _min, _max = self.__initUniformParams()
+        t, _min, _max = self.__init_uniform_params()
 
         # random sample all dimension each time
         while t < self.maxits:
             # sample N data and sort
-            x = self.__uniformSampleData(_min, _max)
-            s = self.__functionReward(instr, x)
-            s = self.__sortSample(s)
+            x = self.__uniform_sample_data(_min, _max)
+            s = self.__function_reward(x)
+            s = self.__sort_sample(s)
             x = np.array([s[i][0] for i in range(np.shape(s)[0])])
-            _min, _max = self.__updateUniformParams(x)
+            _min, _max = self.__update_uniform_params(x)
             t += 1
 
         return (_min + _max) / 2.
 
-    def evalGaussian(self, instr):
+    def eval_gaussian(self):
         # initial parameters
-        t, mu, sigma = self.__initGaussianParams()
+        t, mu, sigma = self.__init_gaussian_params()
 
         # random sample all dimension each time
         while t < self.maxits:
             # sample N data and sort
-            x = self.__gaussianSampleData(mu, sigma)
-            s = self.__functionReward(instr, x)
-            s = self.__sortSample(s)
+            x = self.__gaussian_sample_data(mu, sigma)
+            s = self.__function_reward(x)
+            s = self.__sort_sample(s)
             x = np.array([s[i][0] for i in range(np.shape(s)[0])])
 
             # update parameters
-            mu, sigma = self.__updateGaussianParams(x)
+            mu, sigma = self.__update_gaussian_params(x)
             t += 1
 
         return mu
 
-    def __initGaussianParams(self):
+    def __init_gaussian_params(self):
         """initial parameters t, mu, sigma"""
         t = 0
         mu = np.zeros(self.d)
         sigma = np.ones(self.d) * self.init_coef
         return t, mu, sigma
 
-    def __updateGaussianParams(self, x):
+    def __update_gaussian_params(self, x):
         """update parameters mu, sigma"""
         mu = x[0:self.Ne, :].mean(axis=0)
         sigma = x[0:self.Ne, :].std(axis=0)
         return mu, sigma
 
-    def __gaussianSampleData(self, mu, sigma):
+    def __gaussian_sample_data(self, mu, sigma):
         """sample N examples"""
         sample_matrix = np.zeros((self.N, self.d))
         for j in range(self.d):
@@ -122,29 +120,27 @@ class CrossEntropyMethod(Optimizer):  # TODO
                 sample_matrix[:, j] = np.clip(sample_matrix[:, j], self.v_min[j], self.v_max[j])
         return sample_matrix
 
-    def __initUniformParams(self):
+    def __init_uniform_params(self):
         """initial parameters t, mu, sigma"""
         t = 0
         _min = self.v_min if self.v_min else -np.ones(self.d)
         _max = self.v_max if self.v_max else np.ones(self.d)
         return t, _min, _max
 
-    def __updateUniformParams(self, x):
+    def __update_uniform_params(self, x):
         """update parameters mu, sigma"""
         _min = np.amin(x[0:self.Ne, :], axis=0)
         _max = np.amax(x[0:self.Ne, :], axis=0)
         return _min, _max
 
-    def __uniformSampleData(self, _min, _max):
+    def __uniform_sample_data(self, _min, _max):
         """sample N examples"""
         sample_matrix = np.zeros((self.N, self.d))
         for j in range(self.d):
             sample_matrix[:, j] = np.random.uniform(low=_min[j], high=_max[j], size=(self.N,))
         return sample_matrix
 
-    def __functionReward(self, instr, x):
-        bi = np.reshape(instr, [1, -1])
-        bi = np.repeat(bi, self.N, axis=0)
+    def __function_reward(self, x):
 
         # self.func (fitness_func) should support matrix  operations for faster computations
         value = []
@@ -155,10 +151,27 @@ class CrossEntropyMethod(Optimizer):  # TODO
 
         return zip(x, value)
 
-    def __sortSample(self, s):
+    def __sort_sample(self, s):
         """sort data by function return"""
         s = sorted(s, key=lambda x: x[1], reverse=self.reverse)
 
         return s
 
 
+def sqr(x):
+    # solution x = 2 and y(x) = 3
+    return (x-2) ** 2 + 3
+
+
+def loss_func(x_pred, guess=0):
+    if len(x_pred) > 1:
+        x_pred = x_pred[0]
+    return np.sqrt((sqr(x_pred) - sqr(guess))**2)
+
+
+bds_low = [0, 5]
+bds_high = [5, 7]
+op = CrossEntropyMethod(bds_low, bds_high, loss_func, dim=2)
+solution = op.solve(num_steps=50000)
+
+print(solution)
