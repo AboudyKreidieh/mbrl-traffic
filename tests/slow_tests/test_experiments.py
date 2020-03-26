aa24d628ac769e9d3478b7f6ef5cba2a882b1c32
Aboudy Kreidieh <akreidieh@gmail.com>
2020-03-26 16:56:19 -0700

    evaluate-model (#18)
    * added TODOs
    
    * added initial conditions from simulations
    
    * most of it written
    
    * added way to store initial conditions in a csv
    
    * added tests and some cleanup
    
    * added plot_heatmap
    
    * added video generator
    
    * added tests for plot_only

--- tests/slow_tests/test_experiments.py
+++ tests/slow_tests/test_experiments.py
@@ -2,7 +2,18 @@
 import unittest
 import os
 import shutil
+import numpy as np
 
+from mbrl_traffic.models import LWRModel
+from mbrl_traffic.models import ARZModel
+from mbrl_traffic.models import NoOpModel
+from mbrl_traffic.models import FeedForwardModel
+
+from experiments.evaluate_model import parse_args as parse_args_evaluate_model
+from experiments.evaluate_model import import_data
+from experiments.evaluate_model import get_model_cls
+from experiments.evaluate_model import get_model_ckpt
+from experiments.evaluate_model import main as evaluate_model
 from experiments.simulate import parse_args as parse_args_simulate
 from experiments.train_agent import main as train_agent
 
@@ -24,14 +35,151 @@ class TestEvaluateAgent(unittest.TestCase):
 class TestEvaluateModel(unittest.TestCase):
     """Tests the experiments/evaluate_model.py script."""
 
-    def setUp(self):
-        pass  # TODO
+    def test_parse_args(self):
+        """Validate the functionality of the parse_args() method."""
+        # test the default case
+        args = parse_args_evaluate_model(["."])
+        self.assertDictEqual(args.__dict__, {
+            'checkpoint_num': None,
+            'initial_conditions': None,
+            'plot_only': False,
+            'results_dir': '.',
+            'runs': 1,
+            'save_path': None,
+            'steps': 1000,
+            'svg': False
+        })
 
-    def tearDown(self):
-        pass  # TODO
+        # test the updated case
+        args = parse_args_evaluate_model([
+            '.',
+            '--checkpoint_num', '1',
+            '--initial_conditions', 'initial_conditions',
+            '--plot_only',
+            '--runs', '2',
+            '--save_path', 'save_path',
+            '--steps', '3',
+            '--svg',
+        ])
+        self.assertDictEqual(args.__dict__, {
+            'checkpoint_num': 1,
+            'initial_conditions': 'initial_conditions',
+            'plot_only': True,
+            'results_dir': '.',
+            'runs': 2,
+            'save_path': 'save_path',
+            'steps': 3,
+            'svg': True
+        })
 
-    def test_pass(self):
-        """TODO."""
+    def test_import_data(self):
+        """Validate the functionality of the import_data() method.
+
+        We test this method on a small csv file that is representative of what
+        the method is meant to import.
+        """
+        times, obses = import_data(os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "data/sample_macro.csv"))
+
+        np.testing.assert_almost_equal(times, [0, 0.5, 1])
+        np.testing.assert_almost_equal(obses, [[1,  2,  3,  4],
+                                               [5,  6,  7,  8],
+                                               [9, 10, 11, 12]])
+
+    def test_get_model_cls(self):
+        """Validate the functionality of the get_model_cls() method.
+
+        This method is tested for the following cases:
+
+        1. model = "LWRModel"
+        2. model = "ARZModel"
+        3. model = "NonLocalModel"
+        4. model = "NoOpModel"
+        5. model = "FeedForwardModel"
+        6. model = "dns dl"  <-- returns a ValueError
+        """
+        # test case 1
+        self.assertEqual(get_model_cls("LWRModel"), LWRModel)
+
+        # test case 2
+        self.assertEqual(get_model_cls("ARZModel"), ARZModel)
+
+        # test case 3  TODO
+        # self.assertEqual(get_model_cls("NonLocalModel"), None)
+
+        # test case 4
+        self.assertEqual(get_model_cls("NoOpModel"), NoOpModel)
+
+        # test case 5
+        self.assertEqual(get_model_cls("FeedForwardModel"), FeedForwardModel)
+
+        # test case 6
+        self.assertRaises(ValueError, get_model_cls, model="dns dl")
+
+    def test_get_model_ckpt(self):
+        """Validate the functionality of the get_model_ckpt() method.
+
+        This method is tested for the following cases:
+
+        1. model = "NoOpModel"
+        2. model = "FeedForwardModel"
+        3. model = "LWRModel", ckpt_num = None
+        4. model = "LWRModel", ckpt_num = 1
+        """
+        # test case 1
+        self.assertEqual(get_model_ckpt("base_dir", 1, "NoOpModel"), None)
+
+        # test case 2
+        self.assertEqual(get_model_ckpt("base_dir", 1, "FeedForwardModel"),
+                         "base_dir/checkpoints/itr-1/model.meta")
+
+        # test case 3
+        self.assertEqual(
+            get_model_ckpt(
+                os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)),
+                    "data/sample_log"
+                ),
+                None,
+                "LWRModel"
+            ),
+            os.path.join(
+                os.path.dirname(os.path.realpath(__file__)),
+                "data/sample_log/checkpoints/itr-2/model.csv"
+            )
+        )
+
+        # test case 4
+        self.assertEqual(get_model_ckpt("base_dir", 1, "LWRModel"),
+                         "base_dir/checkpoints/itr-1/model.csv")
+
+    def test_main_plot_only(self):
+        """Validate the functionality of the main() method when plotting."""
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        evaluate_model([os.path.join(cur_dir, "data"), "--plot_only"])
+
+        # Check that the necessary files are generated.
+        self.assertTrue(os.path.isfile(
+            os.path.join(cur_dir, "data/speed_0.png")))
+        self.assertTrue(os.path.isfile(
+            os.path.join(cur_dir, "data/density_0.png")))
+        self.assertTrue(os.path.isfile(
+            os.path.join(cur_dir, "data/flow_0.png")))
+        self.assertTrue(os.path.isfile(
+            os.path.join(cur_dir, "data/speed_0.mp4")))
+        self.assertTrue(os.path.isfile(
+            os.path.join(cur_dir, "data/density_0.mp4")))
+
+        # Check the generated files.
+        os.remove(os.path.join(cur_dir, "data/speed_0.png"))
+        os.remove(os.path.join(cur_dir, "data/density_0.png"))
+        os.remove(os.path.join(cur_dir, "data/flow_0.png"))
+        os.remove(os.path.join(cur_dir, "data/speed_0.mp4"))
+        os.remove(os.path.join(cur_dir, "data/density_0.mp4"))
+
+    def test_main_evaluate(self):
+        """Validate the functionality of the main() method when evaluating."""
         pass  # TODO
 
 
